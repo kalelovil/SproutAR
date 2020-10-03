b91aaa3426625016994112b04a11a5d1c72558a9
Kalelovil <kalelovil@gmail.com>
2020-10-04 09:27:17 +1300

    - fixed tree obj positions

---
+++ Assets/DecorationHolder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecorationHolder : MonoBehaviour
+{
+    [SerializeField] List<Transform> _decorationSlotList = new List<Transform>();
+    [SerializeField] List<GameObject> _decorationList = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/DecorationPlacer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecorationPlacer : MonoBehaviour
+{
+    [SerializeField] List<GameObject> _decorationList = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlaceDecorations()
+    {
+
+    }
+}
