c4d80a39f4543427f3787e85afe0389129c2ad82
Kalelovil <kalelovil@gmail.com>
2020-10-04 10:41:11 +1300

    - decorations added when button clicked

--- Assets/DecorationHolder.cs
+++ Assets/DecorationHolder.cs
@@ -14,7 +14,11 @@ public class DecorationHolder : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        _decorationList = new List<GameObject>();
+        foreach (var slot in _decorationSlotList)
+        {
+            _decorationList.Add(null);
+        }
     }
 
     // Update is called once per frame
@@ -26,14 +30,14 @@ public class DecorationHolder : MonoBehaviour
     internal void AddDecorations(List<GameObject> objs)
     {
         System.Random rand = new System.Random();
-        for (int i = 0; i < objs.Count; )
+        for (int i = 0, timeOut = 0; i < objs.Count && timeOut < 1000; timeOut++)
         {
             int slotIndex = rand.Next(_decorationSlotList.Count);
             var obj = _decorationList[slotIndex];
             if (obj == null)
             {
                 var slot = _decorationSlotList[slotIndex];
-                var gObj = Instantiate(obj, slot);
+                var gObj = Instantiate(objs[i], slot);
                 _decorationList[slotIndex] = gObj;
 
                 i++;
