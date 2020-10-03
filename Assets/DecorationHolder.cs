42dc3948f1eca7ee5cd3ae6ef479e8c52457b9ae
Kalelovil <kalelovil@gmail.com>
2020-10-04 10:40:53 +1300

    - fixed materials

--- Assets/DecorationHolder.cs
+++ Assets/DecorationHolder.cs
@@ -8,6 +8,9 @@ public class DecorationHolder : MonoBehaviour
     [SerializeField] List<Transform> _decorationSlotList = new List<Transform>();
     [SerializeField] List<GameObject> _decorationList = new List<GameObject>();
 
+    [SerializeField] Renderer StemRend;
+    [SerializeField] Renderer LeavesRend;
+
     // Start is called before the first frame update
     void Start()
     {
