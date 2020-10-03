9b000beb3792b82980cb94e3243f96b4f98d211c
Kalelovil <kalelovil@gmail.com>
2020-10-04 07:40:40 +1300

    - scale changer

--- Assets/ScaleChanger.cs
+++ Assets/ScaleChanger.cs
@@ -29,6 +29,25 @@ public class ScaleChanger : MonoBehaviour
         ChangeScale(scaleFactor);
     }
 
+    public void GrowScale(float destinationFactor)
+    {
+        var trans = GameObject.FindGameObjectWithTag("MoneyTree").transform;
+        Vector3 destinationScale = new Vector3(trans.localScale.x * destinationFactor, 
+                                                trans.localScale.y * destinationFactor, 
+                                                trans.localScale.z * destinationFactor);
+        StartCoroutine(AnimateGrowth(trans, destinationScale, _speed));
+    }
+
+    [SerializeField] float _speed;
+    private IEnumerator AnimateGrowth(Transform trans, Vector3 destinationScale, float speed)
+    {
+        while (trans.localScale.x < destinationScale.x || trans.localScale.y < destinationScale.y || trans.localScale.z < destinationScale.z)
+        {
+            yield return null;
+            trans.localScale = Vector3.MoveTowards(trans.localScale, destinationScale, speed * Time.deltaTime);
+        }
+    }
+
     private void ChangeScale(float scaleFactor)
     {
         var trans = GameObject.FindGameObjectWithTag("MoneyTree").transform;
